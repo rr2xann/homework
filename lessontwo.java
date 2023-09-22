468ebd616c758539b5510a966592e7086dceed6a
rr2xan <rr2xannbasic@gmail.com>
2023-09-22 19:38:27 +0300

    lessontwo homework

---
+++ lessontwo.java
@@ -0,0 +1,25 @@
+public class lessontwo {
+    public static void main(String[] args) {
+
+        int a;
+        a = 4 * (17 + 5 - 1) / 2;
+        System.out.println(a);
+
+        { int n = 69;
+            System.out.println(n = 6 + 9); }
+
+        int c = 765;
+        System.out.println(c = 7 + 6 + 5);
+
+        int q = 78;  int w = 6;
+        System.out.println(q / w);
+
+        int z = 6; int x = 4;
+        z = z + x;
+        x = z - x;
+        z = z - x;
+        System.out.println(z);
+        System.out.println(x);
+
+    }
+}
